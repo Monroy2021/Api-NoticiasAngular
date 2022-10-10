9de3fd2963d11d1c89ed5c8081509c803d092d9d
Monroy2021 <luismonroy1014@gmail.com>
2022-10-10 08:45:29 -0500

    Inicializacion del proyecto mas la creacion de los componets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FormularioComponent } from './components/formulario/formulario.component';
+import { ListadonoticiasComponent } from './components/listadonoticias/listadonoticias.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ListadoNoticiasComponent } from './components/listado-noticias/listado-noticias.component';
+import { SpinnerComponent } from './shared/spinner/spinner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormularioComponent,
+    ListadonoticiasComponent,
+    NavbarComponent,
+    ListadoNoticiasComponent,
+    SpinnerComponent
   ],
   imports: [
     BrowserModule
